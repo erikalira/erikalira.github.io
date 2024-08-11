1da31ca70e8656f8e062a7a1db27c54a4e4582f9
Erika Lira <liraerika@gmail.com>
2024-08-11 01:18:12 -0300

    :zap: perf: next image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
